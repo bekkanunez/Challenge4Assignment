0ac54e763a796ff3d6c8a781126ebac2a18498ba
Bekka <music_composer23@hotmail.com>
2022-12-17 00:32:46 -0600

    all updates

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,23 +1,23 @@
 const questions = [
     {
-        Q: 'Question 1',
-        A: ['a','b','c','d'],
-        C: 'b'
+        Q: 'What does HTML Stand for?',
+        A: ['a. Hypertimer Mark Loop','b. Hypertext Markup Language','c. Holographic Text Margin Line','d. Hypertext Mirror Language'],
+        C: 'b. Hypertext Markup Language'
     },
     {
-        Q: 'Question 2',
-        A: ['a','b','c','d'],
-        C: 'b'
+        Q: 'What is CSS used for in web development?',
+        A: ['a. to generate a page','b. to add stye to a page','c. to create a function','d. to protect code'],
+        C: 'b. to add stye to a page'
     },
     {
-        Q: 'Question 3',
-        A: ['a','b','c','d'],
-        C: 'b'
+        Q: 'Which of the following is NOT an html tag?',
+        A: ['a. p','b. body','c. header','d. js'],
+        C: 'd. js'
     },
     {
-        Q: 'Question 4',
-        A: ['a','b','c','d'],
-        C: 'b'
+        Q: 'What does ul stand for',
+        A: ['a. unlisted','b. undefined line','c. unfound link','d. uniform letter'],
+        C: 'a. unlisted'
     },
     {
         Q: 'Question 5',
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,9 @@
 let qI = 0;
 let clockId;
 let time = 60;
+let score = 0;
 const banner = document.querySelector('main');
-
+let store = localStorage.scores ? JSON.parse(localStorage.scores) : [];
 
 const clock = () => {
     time--;
@@ -15,37 +16,66 @@ const clock = () => {
     document.querySelector('.time').innerHTML = time;
 };
 
-const handleAns = (a,b) => {
+const handleAns = a => {
     let cStatus = document.querySelector('.correct');
     let iStatus = document.querySelector('.incorrect');
-    if(a==b) {
+
+    console.log(a,questions[qI].C);
+
+    if(a == questions[qI].C) {
         cStatus.style.display = 'block';
-        setTimeout(() => {cStatus.style.display = 'none'},2000)
+        setTimeout(() => {cStatus.style.display = 'none'},2000);
+        score += 20;
     } else {
         iStatus.style.display = 'block';
         setTimeout(() => {iStatus.style.display = 'none'},2000)
+        time -= 5;
+    };
 
-    }
+    qI++;
+    renderQuestion();
 }
 
 const renderQuestion = () => {
-    const { Q,A,C } = questions[qI];
 
-    banner.innerHTML = `<h1>${Q}</h1>`;
+    if (qI == questions.length) return endQuiz();
+    
+    const { Q,A } = questions[qI];
+
+    banner.innerHTML = `<h1>${Q}</h1><div id="answers"></div>`;
 
     A.forEach(ans => {
-        banner.innerHTML += `<button onclick = "handleAns('${ans}','${C}')"> ${ans}</button>`
+        answers.innerHTML += `<button onclick = "handleAns('${ans}')"> ${ans}</button>`
     });
 }
 
 const start = () => {
     clockId = setInterval(clock,1000);
-
     renderQuestion();
+};
+  
+const endQuiz = () => {
+    clearInterval(clockId);
 
+    banner.innerHTML = `
+        <div id="score">
+            <h1> Your score: ${score}</h1>
+            <h1> Initials: <input onchange="handleIntials()" id="intials" />
+        </div>
+    `;
+};
 
+const handleIntials = () => {
+    store.push({intials: intials.value, score: score })
+    localStorage.scores = JSON.stringify(store);
 
+    banner.innerHTML = '<h1> Highscores: </h1>';
+
+    store.forEach(obj => {
+        Object.values(obj).forEach(val => {
+            console.log(val);
+        })
+    });
 }
-    
 
 document.querySelector('button').addEventListener('click',start)
